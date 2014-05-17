8a0a33b9db66d7ea464cd7d74ca0ea36eba77c29
adrpo <adrpo@20e99eb2-9666-4e79-a4bd-0251279e2d9b>
2014-05-17 06:51:43 +0000

    - remove tools/windows/OMDev/lib/3rdParty/Ipopt as Ipopt is now compiled from 3rdParty/Ipopt during build
    git-svn-id: https://openmodelica.org/svn/OpenModelicaExternal/trunk/tools/windows/OMDev@65 20e99eb2-9666-4e79-a4bd-0251279e2d9b

--- lib/3rdParty/Ipopt/build.sh
+++
@@ -1,4 +0,0 @@
-./configure --prefix=/f/dev/ipopt/ -C --enable-doscompile --disable-shared --enable-static --with-blas-lib="-llapack-mingw -ltmglib-mingw -lblas-mingw -lf2c -L\"\"$OPENMODELICAHOME/lib/omc\"\"" --with-lapack-lib="-llapack-mingw -ltmglib-mingw -lblas-mingw -lf2c -L\"\"$OPENMODELICAHOME/lib/omc\"\"" LDFLAGS='-static'
-make
-make test
-make install
