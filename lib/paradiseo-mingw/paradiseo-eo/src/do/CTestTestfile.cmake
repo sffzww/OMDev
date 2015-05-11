f7a7bdd4915a940b6c0c5b5647a7b6d04aa9fe2f
adeas31 <adeas31@20e99eb2-9666-4e79-a4bd-0251279e2d9b>
2015-05-11 12:31:39 +0000

    - Removed Paradiseo as is build from sources now in new git repository.
    git-svn-id: https://openmodelica.org/svn/OpenModelicaExternal/trunk/tools/windows/OMDev@153 20e99eb2-9666-4e79-a4bd-0251279e2d9b

--- lib/paradiseo-mingw/paradiseo-eo/src/do/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ParadisEO-1.2/paradiseo-eo/src/do
-# Build directory: C:/ParadisEO-1.2/paradiseo-eo/src/do
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
