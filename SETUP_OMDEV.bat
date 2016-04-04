87c85b6679f9912885f207d2c1e44eee8274af4e
adeas31 <adeas31@20e99eb2-9666-4e79-a4bd-0251279e2d9b>
2016-04-04 13:53:07 +0000

    Fixed setup & install files.
    git-svn-id: https://openmodelica.org/svn/OpenModelicaExternal/trunk/tools/windows/OMDev@167 20e99eb2-9666-4e79-a4bd-0251279e2d9b

--- SETUP_OMDEV.bat
+++ SETUP_OMDEV.bat
@@ -1,3 +1,4 @@
+@echo off
 
 if %OMDEV%a==a (goto :OMDEV_NOTOK) else (goto :OMDEV_OK)
 
@@ -10,10 +11,3 @@ REM generate passwd and group
 echo Generate %OMDEV%/tools/msys/etc/passwd and %OMDEV%/tools/msys/etc/group
 %OMDEV%/tools/msys/usr/bin/mkpasswd -l -c > %OMDEV%//tools/msys/etc/passwd
 %OMDEV%/tools/msys/usr/bin/mkgroup -l -c > %OMDEV%//tools/msys/etc/group
-
-REM Fix Qt5 files for the current OMDev installation
-echo Fix Qt5 files for the current OMDev installation
-%OMDEV%/tools/msys/mingw32/bin/qtbinpatcher --nobackup
-%OMDEV%/tools/msys/mingw64/bin/qtbinpatcher --nobackup
-
-
