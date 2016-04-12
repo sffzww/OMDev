12acf968e0c531930f720be2ff2582715dca05e2
adrpo <adrpo@20e99eb2-9666-4e79-a4bd-0251279e2d9b>
2016-04-12 01:15:15 +0000

    - add a batch file to setup qt5
    git-svn-id: https://openmodelica.org/svn/OpenModelicaExternal/trunk/tools/windows/OMDev@182 20e99eb2-9666-4e79-a4bd-0251279e2d9b

---
+++ SETUP_OMDEV_Qt5.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if %OMDEV%a==a (goto :OMDEV_NOTOK) else (goto :OMDEV_OK)
+
+:OMDEV_NOTOK
+echo OMDEV environment variable not defined! Please define it!
+pause
+exit 1
+
+:OMDEV_OK
+REM patch Qt5 files
+echo patch Qt5 files
+cd %OMDEV%/tools/msys/mingw32/bin
+.\qtbinpatcher --nobackup
+cd %OMDEV%/tools/msys/mingw64/bin
+.\qtbinpatcher --nobackup
+
